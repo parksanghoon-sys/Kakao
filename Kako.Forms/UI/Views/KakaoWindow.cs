1b827f6be92eb5f6b03373013b87d671961bfd0b
DESKTOP-L8PQMEN\User <power93266@naver.com>
2023-03-26 22:26:21 +0900

    1. UI 서포트 프로잭트 생성

--- Kako.Forms/UI/Views/KakaoWindow.cs
+++ Kako.Forms/UI/Views/KakaoWindow.cs
@@ -9,5 +9,10 @@ namespace Kako.Forms.UI.Views
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(KakaoWindow), new FrameworkPropertyMetadata(typeof(KakaoWindow)));
         }
+        public KakaoWindow()
+        {
+            //WindowStyle = WindowStyle.None;
+            //AllowsTransparency = true;
+        }
     }
 }
