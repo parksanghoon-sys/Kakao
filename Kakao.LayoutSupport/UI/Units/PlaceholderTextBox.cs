0ddc291147514d20ef6b1511983a84a1edd9dab5
DESKTOP-L8PQMEN\User <power93266@naver.com>
2023-03-27 00:05:36 +0900

    1. 워터마크 텍스트박스 구현 2. 워터마크 텍스트박스를 이용 비밀번호 텍스트박스 구현

---
+++ Kakao.LayoutSupport/UI/Units/PlaceholderTextBox.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Kakao.LayoutSupport.UI.Units
+{
+
+    public class PlaceholderTextBox : TextBox
+    {
+        public static readonly DependencyProperty PlaceholderTextProperty =
+            DependencyProperty.Register(
+                nameof(PlaceholderText),
+                typeof(string),
+                typeof(PlaceholderTextBox), new PropertyMetadata(""));
+        static PlaceholderTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PlaceholderTextBox), new FrameworkPropertyMetadata(typeof(PlaceholderTextBox)));
+        }
+        public string PlaceholderText
+        {
+            get => (string)GetValue(PlaceholderTextProperty);
+            set => SetValue(PlaceholderTextProperty, value);
+        }
+    }
+}
