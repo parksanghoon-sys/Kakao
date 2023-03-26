81decb03133ff507b504c93656db1b2a475f05b3
DESKTOP-L8PQMEN\User <power93266@naver.com>
2023-03-27 01:13:07 +0900

    1. 리스트뷰 컨트롤 생성 2. 2주차 스터디 완료

---
+++ Kakao.Friends.Birth/UI/Units/BirthdayBox.cs
@@ -0,0 +1,13 @@
+﻿using Kakao.LayoutSupport.UI.Units;
+using System.Windows;
+
+namespace Kakao.Friends.Birth.UI.Units
+{
+    public class BirthdayBox : FriendsBox
+    {
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new BirthdaysBoxItem();
+        }
+    }
+}
---
+++ Kakao.LayoutSupport/UI/Units/FriendsBox.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Kakao.LayoutSupport.UI.Units
+{
+    public class FriendsBox : ListBox
+    {
+        static FriendsBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FriendsBox), new FrameworkPropertyMetadata(typeof(FriendsBox)));
+        }
+    }
+}
